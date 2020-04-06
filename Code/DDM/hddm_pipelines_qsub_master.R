e9a7f47cac5b2639c6b5dcd35746b2a418327735
Nathan Tyler Hall <nth7@aci-lgn-007.acib.production.int.aci.ics.psu.edu>
2020-04-06 18:22:59 -0400

    add first prototype of R script that controls qsub -v

---
+++ Code/DDM/hddm_pipelines_qsub_master.R
@@ -0,0 +1,34 @@
+
+# try running an Rscript that submits an array of qsubs using qsub -v --------
+
+
+nodes <- 1
+ppn <- 1
+wt <- 4
+rawdf <- "/gpfs/group/mnh5174/default/Nate/PD_Inhibition_DDM/Data/preprocessed/flanker_accCode.csv"
+out <- "/gpfs/group/mnh5174/default/Nate/HDDM_outputs_PD_Inhibition/samp1000/flanker/clean_sample"
+task <- "flanker"
+job_id <- "test"
+nchains <- 10
+nburn <- 100
+nsamp <- 1000
+models <- paste("v" , 'vsv', 'v_block', 'v_blocksv', 'vst', 'vsvst', 'v_blockst', "v_blocksvst")
+
+
+# system(paste0('qsub -v NODES=\"',nodes,
+#        '\",PPN'=,ppn),
+#        ",WT=", wt,
+#        ",JOB_ID_NH=",job_id,
+#        ",DF=",rawdf,
+#        ",OUTDIR=",out,
+#        ",TASK=",task,
+#        ",MODELS=", models,
+#        ",NCHAINS=", as.character(nchains),
+#        ",NBURN=", as.character(nburn),
+#        ",NSAMP=", as.character(nsamp),
+#        " qsub_inhibition_ddm_args.bash"))
+# 
+# 
+# paste0('qsub -v NODES=\"',nodes),'\",PPN'=,ppn)
+
+paste0('qsub -v NODES=\"',nodes,'\",PPN=\"',ppn,"\" test_script.bash")
