7d4fd925d91d12b831a7b467f7afad9d99f25348
Nate Hall <nate.hall329@gmail.com>
2020-05-25 11:50:29 -0400

    ammend assess_fit.py to be more flexible and pull gng

--- Code/DDM/assess_fit.py
+++ Code/DDM/assess_fit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/Users/natehall/ics/Nate/PD_Inhibition_DDM/Code/DDM/gng_sim_test/hddm_local/bin
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 19 12:46:07 2020
@@ -9,19 +9,19 @@ Created on Thu Mar 19 12:46:07 2020
 ##Read dependencies - some borrowed from Sophie
 import hddm
 import os
-from os import path
+#from os import path
 import pandas as pd
-from pandas import Series
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-import matplotlib.pyplot as plt 
+#from pandas import Series
+#from matplotlib.backends.backend_agg import FigureCanvasAgg
+#import matplotlib.pyplot as plt 
 import pymp
-from patsy import dmatrix
-import kabuki
+#from patsy import dmatrix
+#import kabuki
 from kabuki.analyze import gelman_rubin
-from kabuki.analyze import check_geweke
-import pickle
-import numpy as np
-import sys
+#from kabuki.analyze import check_geweke
+#import pickle
+#import numpy as np
+#import sys
 
 #import time
 
@@ -32,13 +32,25 @@ import sys
 
 ######### model checking and diagnostics'
 
+
+####################################
+######### input parameters
 ## this should follow similar naming conventions to the output of the R master script.
-ics = 0
-tasks = ['flanker', 'recent_probes']
+ics = 1
+tasks = ['flanker']#, 'recent_probes', 'go_nogo']
 full_sample = ['full_sample']#, 'clean_sample'] 
-nsamp = ['samp2000']#, 'samp10000']
+#nsamp = ['samp2000']#, 'samp10000']
+nsamp = ['samp10000']
 code = "acc"
-date = "2020-05-18" #in case you want to just get models from a specific date 
+#date = "2020-05-18" #in case you want to just get models from a specific date 
+use_log = 0
+use_model_csv = 1
+####################################
+#########
+
+
+
+
 
 if ics:
     basedir = '/gpfs/group/mnh5174/default/Nate/PD_Inhibition_DDM'
@@ -49,9 +61,16 @@ else:
 # which models need to be checked?
 
 #pull completed log and see who needs to be diagnosed
-log = pd.read_csv(basedir + '/Code/DDM/pbs_outputs/PD_Inhibition_DDM_job_info_log_completed.csv')
 
-log_finished = log[(log['outputs_located']== 'all')]
+
+if use_log:
+    log = pd.read_csv(basedir + '/Code/DDM/pbs_outputs/PD_Inhibition_DDM_job_info_log_completed.csv')
+    log_finished = log[(log['outputs_located']== 'all')]
+elif use_model_csv:
+    print('reading csv')
+    check_these = pd.read_csv(basedir + '/Data/cache/check_these_models.csv')
+else:
+    print('user must rework script so we know which diagnostics to grab.')
 
 try:
     log_finished = log_finished[(log_finished['DAY_SUB'] == date)]
@@ -66,6 +85,7 @@ except NameError:
 
 # 3/22/20: check model convergence via gelman-rubin r-hat statistic, export traces of one chain, and save table of DIC values for model comparison.
 # 5/6/20: expand to encompass log structure for those models that have finished running completely.
+# 5/25/20: since 
 
 for samp in nsamp:
     print samp
@@ -73,34 +93,56 @@ for samp in nsamp:
         print task
         for samp_size in full_sample:
             print samp_size
-            log_trimmed = log_finished[(log_finished['NSAMP'] == samp) & (log_finished['TASK'] == task) & (log_finished['SAMPLE'] == samp_size)]
-            models = set(list(log_trimmed['MODEL'])) #set function should get unique values to not redo model grabbing
+            
+            if use_log:
+                log_trimmed = log_finished[(log_finished['NSAMP'] == samp) & (log_finished['TASK'] == task) & (log_finished['SAMPLE'] == samp_size)]
+                models = set(list(log_trimmed['MODEL'])) #set function should get unique values to not redo model grabbing
+            elif use_model_csv:
+                x = check_these[(check_these['task'] == task) & (check_these['samples'] == samp)]
+                models = set(list(x['model']))
             
             outputdir = basedir + '/../HDDM_outputs_PD_Inhibition/' + samp + '/' + task + '/' + samp_size + '/model_objects' 
             os.chdir(outputdir)
             print 'Navigated to ' + os.getcwd()
             
             print 'Directory contents:\n'
-            print os.listdir('.')
+            contents = os.listdir('.')
+            print contents
             
-            
+            ## initialize dic df to append to.
             try:
                 dics = pd.read_csv(outputdir+'/../diagnostics/dics_all.csv')[['model', 'DIC']]
             except NameError:
                 dics = []
+               
+                
+                
                 
             for mod in models:
                 print "Assessing model fit for: ", mod
                 
-                nchains = max(log_trimmed['NCHAINS'][log_trimmed['MODEL'] == mod])
-                
+                if use_log:
+                    nchains = max(log_trimmed['NCHAINS'][log_trimmed['MODEL'] == mod])
+                elif use_model_csv:
+#                    chain_list = [x for x in contents if x.startswith(mod + '_chain') & x.endswith('.model')] 
+#                    res = [int(sub.split('_')[1]) for sub in chain_list]
+#                    # using join() + isnumeric() + list comprehension + map() 
+#                    # Extracting numbers from list of strings 
+#                    res = list(map(lambda sub:int(''.join( 
+#                          [ele for ele in sub if ele.isnumeric()])), chain_list)) 
+                    nchains = 9
                 
                 mods = []
+                
             #    with pymp.Parallel(nchains) as ch:
                 
                 for chain in range(0,nchains):
                     print chain
-                    this_model = hddm.load(mod+'_chain'+str(chain) + '_'+ code + 'Code.model') 
+                    try:
+                        this_model = hddm.load(mod+'_chain'+str(chain) + '_'+ code + 'Code.model') 
+                    except ValueError:
+                        print('no string matching' +mod+'_chain'+str(chain) + '_'+ code + 'Code.model')
+                        
                     mods.append(this_model)
                     traces = this_model.get_traces()
                     traces.to_csv(outputdir+'/../diagnostics/'+mod+'_traces_'+ str(chain) +'.csv')
---
+++ Code/DDM/generate_check_these_models.R
@@ -0,0 +1,44 @@
+
+# examine which 10,000 sample chains we need to look through --------------
+
+basedir <- "~/ics/Nate/HDDM_outputs_PD_Inhibition"; setwd(basedir)
+# tasks <- list.dirs(basedir)
+tasks <- c("flanker", "go_nogo", "recent_probes")
+samples <- c("samp10000", "samp2000")
+
+df.all <- data.frame(task = character(0), samples = character(0), model = character(0))
+
+
+for(t in tasks){
+  for(s in samples){
+    mods <- list.files(paste0(basedir, "/", s, "/",   t, "/full_sample/model_objects"))
+    
+    #check to make sure all chains have finished running for all models
+    mds_all <- list()
+    for(i in 0:9){
+      nam <- paste0("chain", i)
+      mds_all[[nam]] <- sub(paste0("_chain",i,"_accCode.model"), "", mods[grepl(paste0("_chain",i,"_accCode.model"), mods)])
+      print(mds_all[[nam]]) 
+      cat("\n")
+      
+    }
+    mds_check <- unique(do.call(c, mds_all))
+    
+    df.check <- data.frame(task = rep(t, length(mds_check)), samples = rep(s, length(mds_check)), model = mds_check )
+    df.all <- rbind(df.all, df.check)
+    
+  }
+    
+}
+
+
+
+
+#trim to specific models from the past that we want to look at
+
+
+
+
+write.csv(df.all, file = "~/ics/Nate/PD_Inhibition_DDM/Data/cache/check_these_models.csv")
+
+
