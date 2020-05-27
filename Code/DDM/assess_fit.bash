b6263aaf88f420c3454ce6a784fe138a228a131b
Nate Hall <nate.hall329@gmail.com>
2020-05-27 11:59:15 -0400

    add kimchi for repickling and add assess_fit qsub script

--- Code/DDM/assess_fit.bash
+++ Code/DDM/assess_fit.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-#PBS -A open
+#PBS -A m5m_a_g_sc_default
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=24:00:00
 #PBS -j oe
 #PBS -M nth7@psu.edu
 #PBS -m abe
-#PBS -N assess_fit
+#PBS -N f10
 
 
 export G=/gpfs/group/mnh5174/default
@@ -18,10 +18,14 @@ cd $PBS_O_WORKDIR
 command -v deactivate >/dev/null 2>&1 && deactivate #exit existing virtual environment if active
 module unload python #make sure no system python modules are loaded
 module use /gpfs/group/mnh5174/default/sw/modules
-#activate python 2.7.9 environment
-source /gpfs/group/mnh5174/default/lab_resources/lab_python/bin/activate
 
+module load python/3.6.3
 
+#activate python 3 environment containing checkout of developer hddm
+source /gpfs/group/mnh5174/default/Nate/PD_Inhibition_DDM/Code/DDM/gng_sim_test/hddm_local/bin/activate
+
+# check that we are in fact using the right version of python
+which python
 
 ######
 ## sample flanker models
@@ -32,9 +36,5 @@ source /gpfs/group/mnh5174/default/lab_resources/lab_python/bin/activate
 #python /gpfs/group/mnh5174/default/Nate/PD_Inhibition_DDM/Code/DDM/run_recent_probes_models.py
 
 #######
-## gelman-rubin checks, chain export, DIC, export 
+## gelman-rubin checks, chain export, DIC, export
 python /gpfs/group/mnh5174/default/Nate/PD_Inhibition_DDM/Code/DDM/assess_fit.py
-
-
-
-
